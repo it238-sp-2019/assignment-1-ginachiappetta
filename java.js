0a50f817b9161c9b51f513dd5d9b910e45164c33
ginachiappetta <49210193+ginachiappetta@users.noreply.github.com>
2019-04-08 19:43:31 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+console.log('Assignment 1 almost done!');
+console.info('This is part of the Assignment');
+var y = 'a string';
+console.log(y);
+
+console.log($);
+    $(document).ready(function() {
+        console.log('ready');
+    });
+
